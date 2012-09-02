7a681a3abdc14d6850a2f560bed0eb4834e780c9
evalentini <evan.valentini@gmail.com>
2012-09-02 13:04:02 -0500

    trying commit again...still problem with untracked files

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.authenticate(params[:login], params[:password])
+    if user
+      session[:user_id] = user.id
+			cookies[:user_id] = user.id
+			cookies[:password] = params[:password]
+      redirect_to_target_or_default root_url, :notice => "Logged in successfully."
+    else
+      flash.now[:alert] = "Invalid login or password."
+      render :action => 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+		cookies[:user_id]=nil
+		flash[:notice] = "You have been logged out"
+    redirect_to root_url, :notice => "You have been logged out."
+  end
+end
